5daf0311d3abd63044e84727b3e336d3ae92c2d5
Otep <josefdrill@gmail.com>
2023-11-27 19:45:34 -0500

    Part 1 - fixed Appetizers page, added REAL flexboxes, made it more readable, removed weird padding issues, maintained format hopefully!

--- scritps/appetizers.js
+++ scritps/appetizers.js
@@ -1,3 +1,4 @@
+// PART 1
 const Appetizers = () => {
   
     const appetizerContent = document.createElement('div');
@@ -6,54 +7,64 @@ const Appetizers = () => {
     const appetizerHeader = document.createElement('h1');
     appetizerHeader.setAttribute('class', 'text-center my-3 pt-5 mb-5');
     appetizerHeader.innerHTML = 'APPETIZERS';
-  
-    const itemContent = document.createElement('div');
-    itemContent.setAttribute('class', 'item-flex');
 
     const appetizerList = document.createElement('ul');
-    appetizerList.setAttribute('class', 'appetizer-list');
+    appetizerList.setAttribute('class', 'appetizer-list')
   
     const appetizerItem = () => {
       const list = document.createElement('li');
-      list.setAttribute('class', 'nav-item list-unstyled mx-2');
+      list.setAttribute('class', 'nav-item list-unstyled mx-2 appetizer-list-item');
   
+      const appetizerColumn1 = document.createElement('div');
+      appetizerColumn1.setAttribute('class', 'appetizer-item-column');
       const appetizer1 = document.createElement('h1');
       appetizer1.setAttribute('id', 'appetizer1');
       appetizer1.setAttribute('class', 'text-center');
       appetizer1.innerHTML = 'DEVILED EGGS';
-      list.appendChild(appetizer1);
       const appetizerParagraph1 = document.createElement('p');
-      appetizerParagraph1.setAttribute('class', 'my-3 pb-3');
+      appetizerParagraph1.setAttribute('class', 'my-3 pb-3 appetizer-text');
       appetizerParagraph1.innerHTML = 'We use the most savory and addiciting recipe to create our unique deviled eggs!';
-      list.appendChild(appetizerParagraph1);
+
+      appetizerColumn1.appendChild(appetizer1);
+      appetizerColumn1.appendChild(appetizerParagraph1);
+      list.appendChild(appetizerColumn1);
   
+      const appetizerColumn2 = document.createElement('div');
+      appetizerColumn2.setAttribute('class', 'appetizer-item-column');
       const appetizer2 = document.createElement('h1');
       appetizer2.setAttribute('id', 'appetizer2');
       appetizer2.setAttribute('class', 'text-center');
       appetizer2.innerHTML = 'GUACAMOLE';
-      list.appendChild(appetizer2);
       const appetizerParagraph2 = document.createElement('p');
-      appetizerParagraph2.setAttribute('class', 'my-3 pb-3');
+      appetizerParagraph2.setAttribute('class', 'my-3 pb-3 appetizer-text');
       appetizerParagraph2.innerHTML = 'Used the most ripest of fruit, combined with the sweetest cream. Good for 2 people.';
-      list.appendChild(appetizerParagraph2);
+
+      appetizerColumn2.appendChild(appetizer2);
+      appetizerColumn2.appendChild(appetizerParagraph2);
+      list.appendChild(appetizerColumn2);
   
+      const appetizerColumn3 = document.createElement('div');
+      appetizerColumn3.setAttribute('class', 'appetizer-item-column');
       const appetizer3 = document.createElement('h1');
       appetizer3.setAttribute('id', 'appetizer3');
       appetizer3.setAttribute('class', 'text-center');
       appetizer3.innerHTML = 'CROSTINI';
       list.appendChild(appetizer3);
       const appetizerParagraph3 = document.createElement('p');
-      appetizerParagraph3.setAttribute('class', 'my-3 pb-3');
+      appetizerParagraph3.setAttribute('class', 'my-3 pb-3 appetizer-text');
       appetizerParagraph3.innerHTML = 'Crisp, golden crostini. Served with a touch of butter and cheese.';
       list.appendChild(appetizerParagraph3);
+
+      appetizerColumn3.appendChild(appetizer3);
+      appetizerColumn3.appendChild(appetizerParagraph3);
+      list.appendChild(appetizerColumn3);
   
       return list;
     };
   
     appetizerContent.appendChild(appetizerHeader);
     appetizerList.appendChild(appetizerItem())
-    itemContent.appendChild(appetizerList)
-    appetizerContent.appendChild(itemContent);
+    appetizerContent.appendChild(appetizerList);
   
     return appetizerContent;
 }
